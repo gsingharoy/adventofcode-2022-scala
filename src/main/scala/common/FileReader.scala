b36277e8dedd70e2dfec18a6a89fdb9bab3e0bae
gsingharoy <gaurav.singha.roy@zalando.de>
2022-12-17 14:57:14 +0100

    Add file reader
    Added an object called FileReader which has FileReader capabilities

---
+++ src/main/scala/common/FileReader.scala
@@ -0,0 +1,16 @@
+package common
+
+import scala.io.Source
+
+object FileReader {
+
+  /**
+   * A function to read a file and return a list of string values
+   *
+   * @param filename string location of the text filename. The file should be in src/main/resources or src/test/resources
+   * @returns a list of strings where each string is a line of the file
+   */
+  def readFile(filename: String): List[String] =
+    Source.fromResource(filename).getLines().toList
+
+}
